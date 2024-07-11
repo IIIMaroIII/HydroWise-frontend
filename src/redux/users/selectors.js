9257bed6422528e1630f366f4f1d16595302242a
Rodion Matvieiev <your.email@example.com>
2024-07-11 19:02:38 -0400

    fixed: eslint rule for prop/types, routes, users initial state with useSelector error in RestrictedRoute

--- src/redux/users/selectors.js
+++ src/redux/users/selectors.js
@@ -1,4 +1,4 @@
-export const selectUser = state => state.auth.user;
-export const selectUserToken = state => state.auth.token;
-export const selectUserIsLoggedIn = state => state.auth.isLoggedIn;
-export const selectRefreshing = state => state.auth.isRefreshing;
+export const selectUser = state => state.users.user;
+export const selectUserToken = state => state.users.token;
+export const selectUserIsLoggedIn = state => state.users.isLoggedIn;
+export const selectRefreshing = state => state.users.isRefreshing;
