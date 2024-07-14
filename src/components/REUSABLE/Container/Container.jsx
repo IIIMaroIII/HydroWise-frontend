d564c276c2295f2d107993c725901a36f2fea6de
Rodion Matvieiev <your.email@example.com>
2024-07-14 10:28:29 -0400

    updated Container.jsx container.module.css

--- src/components/REUSABLE/Container/Container.jsx
+++ src/components/REUSABLE/Container/Container.jsx
@@ -1,9 +1,25 @@
 import clsx from 'clsx';
 import css from './container.module.css';
-import Button from '../Button/Button.jsx';
 
 const Container = ({ children, addClass = '' }) => {
   return <div className={clsx(css.container, addClass)}>{children}</div>;
 };
 
 export default Container;
+
+/*
+<Container addClass={css.desktop}>
+  <div></div>
+  <div></div>
+</Container>
+
+
+<Container
+  addClass={clsx(css.desktop, css.someAdditionalClass, css.anotherClass)}
+>
+  <div></div>
+  <div></div>
+</Container>;
+
+
+*/
