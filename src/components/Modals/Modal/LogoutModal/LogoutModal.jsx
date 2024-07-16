3b7846d6878620df2e5b9164a970d6665779bb51
AnnaViktorivna <anya.zav94@gmail.com>
2024-07-16 16:55:00 +0200

    fixLogOut

--- src/components/Modals/Modal/LogoutModal/LogoutModal.jsx
+++ src/components/Modals/Modal/LogoutModal/LogoutModal.jsx
@@ -1,13 +1,22 @@
 import css from './logoutModal.module.css';
 import { useDispatch } from 'react-redux';
 import { logout } from 'src/redux/users/operations';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
 
 const LogoutModal = () => {
   const dispatch = useDispatch();
   const [open, setOpen] = useState(false);
 
-  const onLogout = () => {
-    dispatch(logout());
+  const handleSubmit = (values, actions) => {
+    dispatch(logout())
+      .unwrap()
+      .then(() => {
+        toast.success('Registration successful!');
+        navigate('/');
+      })
+      .catch(() => toast.error('Oops, Logout went wrong, please try again!'));
   };
 
   const handleCloseModal = () => setOpen(!open);
@@ -16,7 +25,7 @@ const LogoutModal = () => {
     <div>
       <h2 className={css.title}>Log out</h2>
       <p className={css.text}>Do you really want to leave?</p>
-      <button className={css.btn} onClick={onLogout}>
+      <button className={css.btn} onClick={handleSubmit}>
         Log out
       </button>
       <button className={css.btn} close={handleCloseModal}>
