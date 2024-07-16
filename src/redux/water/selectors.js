5179a381bcce58548865f84d4989f1070146e9aa
Rodion Matvieiev <your.email@example.com>
2024-07-16 01:59:09 -0400

    updated res.header with Access-Control-Allow-Origin, Access-Control-Allow-Credentials

--- src/redux/water/selectors.js
+++ src/redux/water/selectors.js
@@ -1,5 +1,5 @@
-export const selectWaterItems = (state) => state.water.water.items;
-export const selectMonthlyWaterItems = (state) => state.water.water.monthlyItems;
-export const selectIsLoading = (state) => state.water.isLoading;
-export const selectIsError = (state) => state.water.error;
-export const selectModalFlags = (state) => state.water.modalFlags;
+export const selectWaterItems = state => state.water.water.dailyItems;
+export const selectMonthlyWaterItems = state => state.water.water.monthlyItems;
+export const selectIsLoading = state => state.water.isLoading;
+export const selectIsError = state => state.water.error;
+export const selectModalFlags = state => state.water.modalFlags;
