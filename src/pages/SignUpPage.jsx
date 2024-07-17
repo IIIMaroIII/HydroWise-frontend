c3867875b8b77ae7fed89beed1b0f3b7d20fd4a9
OlgaIK2023 <144039220+OlgaIK2023@users.noreply.github.com>
2024-07-18 00:54:48 +0300

    Update SignUpPage.jsx

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -3,13 +3,36 @@ import AdvantagesSection from 'src/components/AdvantagesSection/AdvantagesSectio
 import css from './SignUpPage.module.css';
 import Container from 'src/components/REUSABLE/Container/Container';
 
+import { useState, useEffect } from 'react';
+
 const SignUpPage = () => {
+
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  const handleResize = () => {
+    setIsMobile(window.innerWidth <= 1440);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+
   return (
     <Container>
       <div className={css.SignUpContainer}>
-    
-      <SignUpForm />
+
+      {isMobile ? (
+        <SignUpForm />
+      ) : (
+        <>
+          <SignUpForm />
       <AdvantagesSection />
+        </>
+      )}
+    
+      
     </div>
     </Container>
   );
