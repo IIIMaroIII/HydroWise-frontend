9004452dcab372a1e6796785e8ca986c395981b2
Anna01~ <verusiapavlovna@gmail.com>
2024-07-19 20:25:46 +0300

    fix photo imports, add image-object

--- src/components/AdvantagesSection/AdvantagesSection.jsx
+++ src/components/AdvantagesSection/AdvantagesSection.jsx
@@ -2,20 +2,9 @@ import css from './AdvantagesSection.module.css';
 import sprite from '../../assets/pictures/HomePage/sprite.svg';
 import CustomNavLink from '../REUSABLE/CustomNavLink/CustomNavLink';
 import clsx from 'clsx';
-import firstUserMob1x from '../../assets/pictures/HomePage/user-first-mob.webp';
-import firstUserMob2x from '../../assets/pictures/HomePage/user-first-mob@2x.webp';
-import secondUserMob1x from '../../assets/pictures/HomePage/user-second-mob.webp';
-import secondUserMob2x from '../../assets/pictures/HomePage/user-second-mob@2x.webp';
-import thirdUserMob1x from '../../assets/pictures/HomePage/user-third-mob.webp';
-import thirdUserMob2x from '../../assets/pictures/HomePage/user-third-mob@2x.webp';
-import firstUser1x from '../../assets/pictures/HomePage/user-first.webp';
-import firstUser2x from '../../assets/pictures/HomePage/user-first@2x.webp';
-import secondUser1x from '../../assets/pictures/HomePage/user-second.webp';
-import secondUser2x from '../../assets/pictures/HomePage/user-second@2x.webp';
-import thirdUser1x from '../../assets/pictures/HomePage/user-third.webp';
-import thirdUser2x from '../../assets/pictures/HomePage/user-third@2x.webp';
 import { useSelector } from 'react-redux';
 import { selectUserIsLoggedIn } from 'src/redux/users/selectors';
+import { IMAGES } from '../../components/Constants/constants.js';
 
 const AdvantagesSection = ({ onOpenModal }) => {
   const isAuthenticated = useSelector(selectUserIsLoggedIn);
@@ -26,15 +15,15 @@ const AdvantagesSection = ({ onOpenModal }) => {
         <div className={css.homePageCustomersPhotoContainer}>
           <picture>
             <source
-              srcSet={`${firstUser1x} 1x, ${firstUser2x} 2x`}
+              srcSet={`${IMAGES.firstUser1x} 1x, ${IMAGES.firstUser2x} 2x`}
               media="(min-width: 768px)"
             />
             <source
-              srcSet={`${firstUserMob1x} 1x, ${firstUserMob2x} 2x`}
+              srcSet={`${IMAGES.firstUserMob1x} 1x, ${IMAGES.firstUserMob2x} 2x`}
               media="(max-width: 767px)"
             />
             <img
-              src={firstUser1x}
+              src={IMAGES.firstUser1x}
               className={css.homePageCustomersPhoto}
               alt="First user"
               width="47"
@@ -44,15 +33,15 @@ const AdvantagesSection = ({ onOpenModal }) => {
           </picture>
           <picture>
             <source
-              srcSet={`${secondUser1x} 1x, ${secondUser2x} 2x`}
+              srcSet={`${IMAGES.secondUser1x} 1x, ${IMAGES.secondUser2x} 2x`}
               media="(min-width: 768px)"
             />
             <source
-              srcSet={`${secondUserMob1x} 1x, ${secondUserMob2x} 2x`}
+              srcSet={`${IMAGES.secondUserMob1x} 1x, ${IMAGES.secondUserMob2x} 2x`}
               media="(max-width: 767px)"
             />
             <img
-              src={secondUser1x}
+              src={IMAGES.secondUser1x}
               className={clsx(css.homePageCustomersPhoto, css.secondUserPhoto)}
               alt="Second user"
               width="47"
@@ -62,15 +51,15 @@ const AdvantagesSection = ({ onOpenModal }) => {
           </picture>
           <picture>
             <source
-              srcSet={`${thirdUser1x} 1x, ${thirdUser2x} 2x`}
+              srcSet={`${IMAGES.thirdUser1x} 1x, ${IMAGES.thirdUser2x} 2x`}
               media="(min-width: 768px)"
             />
             <source
-              srcSet={`${thirdUserMob1x} 1x, ${thirdUserMob2x} 2x`}
+              srcSet={`${IMAGES.thirdUserMob1x} 1x, ${IMAGES.thirdUserMob2x} 2x`}
               media="(max-width: 767px)"
             />
             <img
-              src={thirdUser1x}
+              src={IMAGES.thirdUser1x}
               className={clsx(css.homePageCustomersPhoto, css.thirdUserPhoto)}
               alt="Third user"
               width="47"
