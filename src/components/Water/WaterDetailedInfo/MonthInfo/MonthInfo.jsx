42953330b8d97739b69bdad1b3097f11f176abea
Anna01~ <verusiapavlovna@gmail.com>
2024-07-19 20:19:16 +0300

    add library, icon, chart

--- src/components/Water/WaterDetailedInfo/MonthInfo/MonthInfo.jsx
+++ src/components/Water/WaterDetailedInfo/MonthInfo/MonthInfo.jsx
@@ -1,18 +1,39 @@
 import { Calendar } from './Calendar/Calendar';
 import { CalendarPagination } from './CalendarPagination/CalendarPagination';
-// import sprite from '../../../shared/icons/index';
-
+import sprite from '../../../../assets/pictures/HomePage/sprite.svg';
 import css from './MonthInfo.module.css';
+import { useState } from 'react';
+import ChartComponent from 'src/components/Statistics/ChartComponent';
 
-export const MonthInfo = () => {
+const MonthInfo = () => {
+  const [showChart, setShowChart] = useState(false);
+
+  const handleIconClick = () => {
+    setShowChart(!showChart);
+  };
+
   return (
     <>
       <div className={css.wrapper}>
-        <h3 className={css.month}>Month</h3>
+        {showChart ? (
+          <h3 className={css.month}>Statistics</h3>
+        ) : (
+          <h3 className={css.month}>Month</h3>
+        )}
         <CalendarPagination />
+        <svg
+          className={css.monthInfoIcon}
+          width="20"
+          height="20"
+          onClick={handleIconClick}
+          style={{ cursor: 'pointer' }}
+        >
+          <use href={`${sprite}#icon-pie-chart-02`}></use>
+        </svg>
       </div>
-      <Calendar />
+      {showChart ? <ChartComponent /> : <Calendar />}
     </>
   );
 };
+
 export default MonthInfo;
