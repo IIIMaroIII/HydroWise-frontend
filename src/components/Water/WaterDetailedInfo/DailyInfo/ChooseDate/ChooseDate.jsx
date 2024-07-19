2ae2dbe25059de7ba603b1cbf5fbf8be65de4156
Escbrk <69437294+Escbrk@users.noreply.github.com>
2024-07-19 00:39:03 -0600

    upd ChooseDate.jsx : 1. Added function for rendering which day and date is chosen

--- src/components/Water/WaterDetailedInfo/DailyInfo/ChooseDate/ChooseDate.jsx
+++ src/components/Water/WaterDetailedInfo/DailyInfo/ChooseDate/ChooseDate.jsx
@@ -1,5 +1,37 @@
+import { parseISO } from 'date-fns';
+import { useSelector } from 'react-redux';
+import { selectDate } from 'src/redux/water/selectors.js';
+
 const ChooseDate = () => {
-  return <div>Today</div>;
+  const selectedDate = useSelector(selectDate);
+  const days = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Sunday',
+    'Saturday',
+  ];
+  const parsedDate = new Date(parseISO(selectedDate));
+  const date = new Date();
+
+  const ifToday = () => {
+    const formatedDate = date => {
+      const year = date.getFullYear();
+      const month = date.getMonth() + 1;
+      const day = date.getDate();
+
+      return `${year}/${month}/${day}`;
+    };
+    return formatedDate(parsedDate) === formatedDate(date);
+  };
+
+  const chosenDay = days[parsedDate.getDay()];
+
+  return (
+    <div>{ifToday() ? 'Today' : `${parsedDate.getDate()}, ${chosenDay}`}</div>
+  );
 };
 
 export default ChooseDate;
