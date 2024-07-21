cdd80fa4791bcc09f4891e5d16c3affb54e92853
Rodion Matvieiev <your.email@example.com>
2024-07-21 17:14:58 -0400

    added useEffect onto WaterDetailedInfo for fetching data

--- src/components/Water/WaterDetailedInfo/WaterDetailedInfo.jsx
+++ src/components/Water/WaterDetailedInfo/WaterDetailedInfo.jsx
@@ -2,8 +2,20 @@ import UserPanel from 'src/components/Users/UserPanel/UserPanel.jsx';
 import css from './WaterDetailedInfo.module.css';
 import MonthInfo from './MonthInfo/MonthInfo.jsx';
 import DailyInfo from './DailyInfo/DailyInfo.jsx';
+import { useDispatch } from 'react-redux';
+import {
+  fetchDailyWater,
+  fetchMonthlyWater,
+} from 'src/redux/water/operations.js';
+import { useEffect } from 'react';
 
 const WaterDetailedInfo = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchDailyWater());
+    dispatch(fetchMonthlyWater());
+  }, [dispatch]);
+
   return (
     <section className={css.wrapper}>
       <UserPanel />
