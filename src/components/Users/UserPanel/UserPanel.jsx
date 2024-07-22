9b9748fa62361edfe8988edc38cd9f57fa3284d6
Escbrk <69437294+Escbrk@users.noreply.github.com>
2024-07-21 21:22:58 -0600

    upd

--- src/components/Users/UserPanel/UserPanel.jsx
+++ src/components/Users/UserPanel/UserPanel.jsx
@@ -1,15 +1,17 @@
 import { useSelector } from 'react-redux';
 
-import { selectUser } from 'src/redux/users/selectors';
+import { selectUser, selectUserIsLoggedIn } from 'src/redux/users/selectors';
 import UserBar from './UserBar/UserBar.jsx';
 import css from './UserPanel.module.css';
 const UserPanel = () => {
   const user = useSelector(selectUser);
+  const isLoggedIn = useSelector(selectUserIsLoggedIn);
 
   return (
     <div className={css.user_panel}>
-      {/* <div>Hello, dear {!user.name ? <p>User</p> : <p>{user.name}</p>}</div> */}
-      <div>Hello, dear {<span>{user?.name}</span> ?? <span>User</span>}</div>
+      <div>
+        Hello, dear {isLoggedIn ? <span>{user?.name}</span> : <span>User</span>}
+      </div>
       <UserBar />
     </div>
   );
