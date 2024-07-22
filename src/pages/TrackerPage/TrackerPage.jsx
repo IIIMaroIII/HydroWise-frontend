8201b3b51ba316a35ce48bf90b7d05545d57935b
Escbrk <69437294+Escbrk@users.noreply.github.com>
2024-07-21 21:59:28 -0600

    upd

--- src/pages/TrackerPage/TrackerPage.jsx
+++ src/pages/TrackerPage/TrackerPage.jsx
@@ -1,14 +1,8 @@
-import toast from 'react-hot-toast';
-import { useDispatch, useSelector } from 'react-redux';
-import Button from 'src/components/REUSABLE/Button/Button.jsx';
+import { useDispatch } from 'react-redux';
 import WaterDetailedInfo from 'src/components/Water/WaterDetailedInfo/WaterDetailedInfo.jsx';
 import WaterMainInfo from 'src/components/Water/WaterMainInfo/WaterMainInfo.jsx';
-import { refresh } from 'src/redux/users/operations.js';
-import { fetchDailyWater, fetchMonthlyWater } from 'src/redux/water/operations';
-import { selectDate } from 'src/redux/water/selectors';
-import { parseDate } from 'src/utils/parseDate';
+
 import css from './TrackerPage.module.css';
-import Logo from 'src/components/REUSABLE/Logo/Logo';
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { setShowChart } from 'src/redux/chart/slice';
@@ -24,21 +18,6 @@ const TrackerPage = () => {
       dispatch(setShowChart(false));
     }
   }, [location.pathname, dispatch]);
-  // const selectedDate = useSelector(selectDate);
-  // const { day, month, year } = parseDate(selectedDate);
-
-  // useEffect(() => {
-
-  //   dispatch(fetchDailyWater({ day, month, year }));
-  //   dispatch(fetchMonthlyWater({month, year }));
-  // }, [dispatch, day, month, year]);
-
-  const handleOnClick = () => {
-    dispatch(refresh())
-      .unwrap()
-      .then(res => toast.success(res.message))
-      .catch(err => toast.error(err.message));
-  };
 
   return (
     <div className={css.container}>
