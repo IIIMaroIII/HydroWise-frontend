8b3c1de023cad03fa8c3f8fa8677641b4eba70e5
Escbrk <69437294+Escbrk@users.noreply.github.com>
2024-07-29 23:11:48 -0600

    upd

--- src/components/Water/WaterDetailedInfo/MonthInfo/MonthInfo.jsx
+++ src/components/Water/WaterDetailedInfo/MonthInfo/MonthInfo.jsx
@@ -1,6 +1,6 @@
 import { Calendar } from './Calendar/Calendar';
 import { CalendarPagination } from './CalendarPagination/CalendarPagination';
-import sprite from '/public/sprite.svg';
+import sprite from '/src/assets/pictures/HomePage/sprite.svg';
 import css from './MonthInfo.module.css';
 
 const MonthInfo = () => {
